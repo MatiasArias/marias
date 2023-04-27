626fa6a1f56645427e781ef0aad386d6a7bc9420
Matias Arias <matiasarias384@gmail.com>
2023-04-27 13:54:25 -0300

    Add HttpMethod for TechnologyByCandidate controller
    GETbyId, DELETE, PUT, POST

--- src/main/java/org/mobydigital/marias/testbackenddeveloper/controllers/TechnologyByCandidateController.java
+++ src/main/java/org/mobydigital/marias/testbackenddeveloper/controllers/TechnologyByCandidateController.java
@@ -6,10 +6,7 @@ import org.mobydigital.marias.testbackenddeveloper.models.entities.TechnologyByC
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +20,25 @@ public class TechnologyByCandidateController {
     public ResponseEntity<List<TechnologyByCandidate>> getTechnologiesByCandidate(@PathVariable("id") Long id){
         return new ResponseEntity<>(technologyByCandidateService.findAll(id), HttpStatus.OK);
     }
+    @GetMapping("/{idTechnologyByCandidate}")
+    public ResponseEntity<TechnologyByCandidate> getTechnologiesByCandidateById(@PathVariable("idTechnologyByCandidate") Long id){
+        return new ResponseEntity<>(technologyByCandidateService.getTechnologyById(id), HttpStatus.OK);
+    }
     
+    @PostMapping("/create")
+    public ResponseEntity<TechnologyByCandidate> createTechnologyByCandidate(@RequestBody TechnologyByCandidate technologyByCandidate){
+       return new ResponseEntity<>(technologyByCandidateService.createTechnologyByCandidate(technologyByCandidate),HttpStatus.OK);
+    }
+
+    @PutMapping("/update/{idTechnologyByCandidate}")
+    public ResponseEntity updateTechnologyByCandidate(@PathVariable("idTechnologyByCandidate") Long id, @RequestBody TechnologyByCandidate technologyByCandidate){
+        technologyByCandidateService.updateTechnologyByCandidate(id,technologyByCandidate);
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{idTechnologyByCandidate}")
+    public ResponseEntity deleteTechnologyByCandidate(@PathVariable("idTechnologyByCandidate") Long id){
+        technologyByCandidateService.deleteTechnologyByCandidate(id);
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
 }
