bdca889de3a54d503c7c15f48cc65bc42209545b
Matias Arias <matiasarias384@gmail.com>
2023-04-27 16:29:45 -0300

    Add DeleteAction.CASCADE

--- src/main/java/org/mobydigital/marias/testbackenddeveloper/models/entities/TechnologyByCandidate.java
+++ src/main/java/org/mobydigital/marias/testbackenddeveloper/models/entities/TechnologyByCandidate.java
@@ -2,6 +2,8 @@ package org.mobydigital.marias.testbackenddeveloper.models.entities;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 @Entity
 @Table(name="technology_by_candidate")
@@ -15,9 +17,12 @@ public class TechnologyByCandidate {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @ManyToOne()
+    @OnDelete(action = OnDeleteAction.CASCADE)
     @JoinColumn(name = "id_candidate")
     private Candidate candidate;
+
     @ManyToOne()
+    @OnDelete(action = OnDeleteAction.CASCADE)
     @JoinColumn(name = "id_technology")
     private Technology technology;
     private Integer yearsOfExperience;
