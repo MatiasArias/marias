2e718db7085e01121bf32ef402a7d59f32b2ded1
Matias Arias <matiasarias384@gmail.com>
2023-04-27 10:00:49 -0300

    Add JPA and Lombok to Candidate

--- src/main/java/org/mobydigital/marias/testbackenddeveloper/models/entities/Candidate.java
+++ src/main/java/org/mobydigital/marias/testbackenddeveloper/models/entities/Candidate.java
@@ -1,11 +1,21 @@
 package org.mobydigital.marias.testbackenddeveloper.models.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
 import org.mobydigital.marias.testbackenddeveloper.models.enums.DocumentTypeEnum;
 
 import java.util.Date;
-
+@Entity
+@Table(name="candidate")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Candidate {
-    private long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long idCandidate;
     private String name;
     private String lastname;
     private DocumentTypeEnum documentType;
