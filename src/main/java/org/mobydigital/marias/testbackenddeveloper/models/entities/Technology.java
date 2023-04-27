5cb3a02c425d2fbb7638c514a1fc840138cfffbc
Matias Arias <matiasarias384@gmail.com>
2023-04-27 10:05:49 -0300

    Add JPA and Lombok to Technology

--- src/main/java/org/mobydigital/marias/testbackenddeveloper/models/entities/Technology.java
+++ src/main/java/org/mobydigital/marias/testbackenddeveloper/models/entities/Technology.java
@@ -1,6 +1,18 @@
 package org.mobydigital.marias.testbackenddeveloper.models.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name="technology")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Technology {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String version;
