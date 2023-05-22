50e9a5b87cba9a48b997206fa016be77c7752a46
Matias Arias <marias@mobydigital.com>
2023-05-22 14:51:57 -0300

    Create TechnologyDto

---
+++ src/main/java/org/mobydigital/marias/testbackenddeveloper/model/view/TechnologyDto.java
@@ -0,0 +1,10 @@
+package org.mobydigital.marias.testbackenddeveloper.model.view;
+
+import lombok.Data;
+
+@Data
+public class TechnologyDto {
+    private Long idTechnology;
+    private String name;
+    private String version;
+}
