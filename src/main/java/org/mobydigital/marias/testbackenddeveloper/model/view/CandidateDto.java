b6fcc2056901e187bc85db151225e83941cbc5fc
Matias Arias <marias@mobydigital.com>
2023-05-22 14:51:40 -0300

    Create CandidateDto

---
+++ src/main/java/org/mobydigital/marias/testbackenddeveloper/model/view/CandidateDto.java
@@ -0,0 +1,16 @@
+package org.mobydigital.marias.testbackenddeveloper.model.view;
+
+import lombok.Data;
+import org.mobydigital.marias.testbackenddeveloper.model.enums.DocumentTypeEnum;
+
+import java.util.Date;
+@Data
+public class CandidateDto {
+
+    private long idCandidate;
+    private String name;
+    private String lastname;
+    private DocumentTypeEnum documentType;
+    private Integer documentNumber;
+    private Date birthdate;
+}
