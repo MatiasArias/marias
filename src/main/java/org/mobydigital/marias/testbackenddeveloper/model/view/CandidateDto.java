d6184f6c6eba0439fb4bf9eeae7fa6358fa884f2
Matias Arias <marias@mobydigital.com>
2023-05-24 15:33:08 -0300

    Upgrade DTOs

--- src/main/java/org/mobydigital/marias/testbackenddeveloper/model/view/CandidateDto.java
+++ src/main/java/org/mobydigital/marias/testbackenddeveloper/model/view/CandidateDto.java
@@ -1,10 +1,16 @@
 package org.mobydigital.marias.testbackenddeveloper.model.view;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.mobydigital.marias.testbackenddeveloper.model.enums.DocumentTypeEnum;
 
 import java.util.Date;
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class CandidateDto {
 
     private String name;
--- src/main/java/org/mobydigital/marias/testbackenddeveloper/model/view/TechnologyByCandidateDto.java
+++ src/main/java/org/mobydigital/marias/testbackenddeveloper/model/view/TechnologyByCandidateDto.java
@@ -1,10 +1,16 @@
 package org.mobydigital.marias.testbackenddeveloper.model.view;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class TechnologyByCandidateDto {
     private Long id;
     private Long idCandidate;
