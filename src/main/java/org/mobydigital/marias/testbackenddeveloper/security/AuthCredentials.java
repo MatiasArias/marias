950d844500373939c4638ee0e17100f99089fabc
Matias Arias <marias@mobydigital.com>
2023-05-30 20:56:10 -0300

    Add Token,Credentials and Filter

---
+++ src/main/java/org/mobydigital/marias/testbackenddeveloper/security/AuthCredentials.java
@@ -0,0 +1,9 @@
+package org.mobydigital.marias.testbackenddeveloper.security;
+
+import lombok.Data;
+
+@Data
+public class AuthCredentials {
+    private String username;
+    private String password;
+}
---
+++ src/main/java/org/mobydigital/marias/testbackenddeveloper/security/TokenUtils.java
@@ -0,0 +1,40 @@
+package org.mobydigital.marias.testbackenddeveloper.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.mobydigital.marias.testbackenddeveloper.model.entity.User;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TokenUtils {
+    private final static String ACCESS_TOKEN_SECRET = "mzwTYSJd?bHmZ7sY!wR3LKLkExjzDzI1WU8lRvq1uyCc0sGhsihWuSqPt5EdNwtL";
+    private final static Long ACCESS_TOKEN_VALIDITY_SECONDS = 2_592_000L;
+
+    public static String createToken(String username, String email){
+        long expirationTime = ACCESS_TOKEN_VALIDITY_SECONDS*1000;
+        Date expirationDate = new Date(System.currentTimeMillis()+expirationTime);
+        Map<String,Object> extra = new HashMap<>();
+        extra.put("username",username);
+        return Jwts.builder()
+                .setSubject(email)
+                .setExpiration(expirationDate)
+                .addClaims(extra)
+                .signWith(Keys.hmacShaKeyFor(ACCESS_TOKEN_SECRET.getBytes()))
+                .compact();
+    }
+
+    public static UsernamePasswordAuthenticationToken getAuthentication(String token){try{
+        Claims claims = Jwts.parserBuilder().setSigningKey(ACCESS_TOKEN_SECRET.getBytes()).build().parseClaimsJws(token).getBody();
+        String email = claims.getSubject();
+        return new UsernamePasswordAuthenticationToken(email,null, Collections.emptyList());
+    }catch(JwtException e){
+        return null;
+    }
+    }
+}
